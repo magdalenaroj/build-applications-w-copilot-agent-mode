ce254b382f67a24678ae4ede5cc449e6cdcf5f1e
mmsrojm <rojm@media-saturn.com>
2025-12-30 08:30:37 +0100

    fix for codespace urls

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+
+# Domain suffix used for Codespaces URLs, as referenced in views.py
+DOMAIN_SUFFIX = "-8000.app.github.dev"
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
